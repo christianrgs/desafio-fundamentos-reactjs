5e8afe23eec30d8a050ee24c5d56260292b10bfe
Christian Gonçalves <christiangoncalvesr@gmail.com>
2020-11-20 14:32:12 -0300

    Improve

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -52,10 +52,7 @@ const Dashboard: React.FC = () => {
 
         return {
           ...transaction,
-          formattedValue:
-            transaction.type === 'outcome'
-              ? `- ${formattedValue}`
-              : formattedValue,
+          formattedValue,
           formattedDate,
         };
       },
@@ -119,6 +116,7 @@ const Dashboard: React.FC = () => {
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
                     {transaction.formattedValue}
                   </td>
                   <td>{transaction.category.title}</td>
